5762b6af9a186190cd4e5e9ef8fa0db70c601a00
Ewer <136239523+Ewerall@users.noreply.github.com>
2025-07-26 00:39:04 +0300

    Rework Selenium tests
    Add more tests, rework last added, add allure report

--- Auto-testing/UI-Tests/login_page.py
+++ Auto-testing/UI-Tests/login_page.py
@@ -1,19 +1,24 @@
 from selenium.webdriver.common.by import By 
+import allure
 
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
         self.url = "https://www.saucedemo.com/"
-        
+    
+    @allure.step("Открыть страницу авторизации")
     def open(self):
         self.driver.get(self.url)
         
+    @allure.step("Ввести пароль и логин: {username}/{password}")    
     def enter_credentials(self, username, password):
         self.driver.find_element(By.ID, "user-name").send_keys(username)
         self.driver.find_element(By.ID, "password").send_keys(password)
         
+    @allure.step("Нажать кнопку логин")    
     def click_login(self):
         self.driver.find_element(By.ID, "login-button").click()
         
+    @allure.step("Проверить авторизацию")    
     def is_logged_in(self):
         return "inventory" in self.driver.current_url
---
+++ Auto-testing/UI-Tests/product_page.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+import allure
+
+class ProductPage:
+    def __init__(self, driver):
+        self.driver = driver
+        
+    @allure.step("Открыта ли страница продукта")    
+    def is_page_opened(self):
+        return "inventory-item.html" in self.driver.current_url and "id=" in self.driver.current_url
+        
+    @allure.step("Получить имя продукта")    
+    def get_product_name(self):
+        return self.driver.find_element(By.CLASS_NAME, "inventory_details_name").text
+        
+    @allure.step("Получить цену продукта")    
+    def get_product_price(self):
+        return self.driver.find_element(By.CLASS_NAME, "inventory_details_price").text
